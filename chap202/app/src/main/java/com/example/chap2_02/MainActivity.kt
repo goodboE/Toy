d079d71902c6b8fd7e775255469ae19c9cc03eef
HyunJin Ko <iamkokoko123@gmail.com>
2022-12-27 22:55:35 +0900

    finish

--- chap202/app/src/main/java/com/example/chap2_02/MainActivity.kt
+++ chap202/app/src/main/java/com/example/chap2_02/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.chap2_02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.chap2_02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private var startNumber = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mBinding.root)
+
+        initButtons()
+
     }
+
+    private fun initButtons() {
+
+        mBinding.resetButton.setOnClickListener {
+            startNumber = 0
+            setNumberText()
+            Log.d("onClick", "reset!! number=$startNumber")
+        }
+
+        mBinding.plusButton.setOnClickListener {
+            startNumber+=1
+            setNumberText()
+            Log.d("onClick", "plus!! number=$startNumber")
+        }
+    }
+
+    private fun setNumberText() {
+        mBinding.numberTextView.text = startNumber.toString()
+    }
+
+
 }
