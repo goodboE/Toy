31ba168f6c94a58923368616a7ef28b4af9e9346
HyunJin Ko <iamkokoko123@gmail.com>
2023-07-13 21:37:17 +0900

    make recyclerview

--- fcam_news/app/src/main/java/com/toy/fcam_news/MainActivity.kt
+++ fcam_news/app/src/main/java/com/toy/fcam_news/MainActivity.kt
@@ -3,14 +3,19 @@ package com.toy.fcam_news
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import com.toy.fcam_news.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var newsAdapter: NewsAdapter
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://news.google.com/")
@@ -24,12 +29,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        newsAdapter = NewsAdapter()
+
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = newsAdapter
+        }
+
 
         val newsService = retrofit.create(NewsService::class.java)
         newsService.mainFeed().enqueue(object: Callback<NewsRss> {
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("MainActivity", "${response.body()?.channel?.items}")
+
+                newsAdapter.submitList(response.body()?.channel?.items.orEmpty())
             }
 
             override fun onFailure(call: Call<NewsRss>, t: Throwable) {
---
+++ fcam_news/app/src/main/java/com/toy/fcam_news/NewsAdapter.kt
@@ -0,0 +1,43 @@
+package com.toy.fcam_news
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.toy.fcam_news.databinding.ItemNewsBinding
+
+class NewsAdapter : ListAdapter<NewsItem, NewsAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val binding: ItemNewsBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: NewsItem) {
+            binding.titleTextView.text = item.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemNewsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<NewsItem>() {
+            override fun areItemsTheSame(oldItem: NewsItem, newItem: NewsItem): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: NewsItem, newItem: NewsItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
