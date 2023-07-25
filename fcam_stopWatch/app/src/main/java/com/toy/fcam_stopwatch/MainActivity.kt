65361fd91860a8e47a0e406b7083506b83356097
HyunJin Ko <iamkokoko123@gmail.com>
2023-07-25 20:52:37 +0900

    finish

--- fcam_stopWatch/app/src/main/java/com/toy/fcam_stopwatch/MainActivity.kt
+++ fcam_stopWatch/app/src/main/java/com/toy/fcam_stopwatch/MainActivity.kt
@@ -1,23 +1,46 @@
 package com.toy.fcam_stopwatch
 
+import android.annotation.SuppressLint
+import android.media.AudioManager
+import android.media.ToneGenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
+import androidx.core.view.setPadding
 import com.toy.fcam_stopwatch.databinding.ActivityMainBinding
+import com.toy.fcam_stopwatch.databinding.DialogCountdownSettingBinding
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var countdownSecond = 10
+    private var countdownSecond = 5
+    private var currentCountdownDeciSecond = countdownSecond * 10
+    private var currentDeciSecond = 0
+    private var timer: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.countdownTextView.setOnClickListener {
+        initViews()
 
+    }
+
+    private fun initViews() {
+
+        val seconds = currentCountdownDeciSecond / 10
+        binding.countdownTextView.text = String.format("%02d", seconds)
+        binding.countdownProgressBar.progress = 100
+
+        binding.countdownTextView.setOnClickListener {
+            showCountDownSettingDialog()
         }
 
         binding.startButton.setOnClickListener {
@@ -34,6 +57,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.pauseButton.setOnClickListener {
+            pause()
             binding.startButton.isVisible = true
             binding.stopButton.isVisible = true
             binding.pauseButton.isVisible = false
@@ -43,17 +67,52 @@ class MainActivity : AppCompatActivity() {
         binding.lapButton.setOnClickListener {
             lap()
         }
-
-
-
     }
 
     private fun start() {
 
+        // created thread
+        timer = timer(initialDelay = 0, period = 100) {
+
+            if (currentCountdownDeciSecond == 0) {
+                currentDeciSecond += 1
+
+                val minutes = currentDeciSecond.div(10) / 60
+                val seconds = currentDeciSecond.div(10) % 60
+                val deciSeconds = currentDeciSecond % 10
+
+                runOnUiThread {
+                    binding.timeTextView.text =
+                        String.format("%02d:%02d", minutes, seconds)
+                    binding.tickTextView.text = deciSeconds.toString()
+
+                    binding.countdownGroup.isVisible = false
+
+                }
+            } else {
+                currentCountdownDeciSecond -= 1
+                val seconds = currentCountdownDeciSecond / 10
+                val progress = (currentCountdownDeciSecond / (countdownSecond * 10f)) * 100
+
+                binding.root.post {
+                    binding.countdownTextView.text = String.format("%02d", seconds)
+                    binding.countdownProgressBar.progress = progress.toInt()
+                }
+
+            }
+
+            if (currentDeciSecond == 0 && currentCountdownDeciSecond < 31 && currentCountdownDeciSecond % 10 == 0) {
+                val toneType = if(currentCountdownDeciSecond == 0) ToneGenerator.TONE_CDMA_HIGH_L else ToneGenerator.TONE_CDMA_ANSWER
+                ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME)
+                    .startTone(toneType, 100)
+                Log.d("alarm", toneType.toString())
+            }
+        }
     }
 
     private fun pause() {
-        stop()
+        timer?.cancel()
+        timer = null
     }
 
     private fun stop() {
@@ -61,18 +120,59 @@ class MainActivity : AppCompatActivity() {
         binding.stopButton.isVisible = true
         binding.pauseButton.isVisible = false
         binding.lapButton.isVisible = false
+
+        currentDeciSecond = 0
+        binding.timeTextView.text = "00:00"
+        binding.tickTextView.text = "0"
+        binding.countdownGroup.isVisible = true
+        binding.countdownTextView.text = String.format("%02d", countdownSecond)
+        binding.countdownProgressBar.progress = 100
+        binding.containerLinearLayout.removeAllViews()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun lap() {
+
+        if (currentDeciSecond == 0) return
+
+        val container = binding.containerLinearLayout
+        TextView(this).apply {
+            textSize = 20f
+            gravity = Gravity.CENTER
+
+            val minutes = currentDeciSecond.div(10) / 60
+            val seconds = currentDeciSecond.div(10) % 60
+            val deciSeconds = currentDeciSecond % 10
+
+            // 1. 01:03 0
+            text = "${container.childCount.inc()} => " + String.format(
+                "%02d:%02d %01d",
+                minutes, seconds, deciSeconds
+            )
+            setPadding(30)
+        }.let { lapTextView ->
+            container.addView(lapTextView, 0)
+        }
+
 
     }
 
     private fun showCountDownSettingDialog() {
         AlertDialog.Builder(this).apply {
+            val dialogBinding = DialogCountdownSettingBinding.inflate(layoutInflater)
+
+            with(dialogBinding.countdownSecondPicker) {
+                maxValue = 20
+                minValue = 0
+                value = countdownSecond
+            }
+            setTitle("카운트다운 설정")
+            setView(dialogBinding.root)
+
             setPositiveButton("확인") { _, _ ->
-
-
-
+                countdownSecond = dialogBinding.countdownSecondPicker.value
+                currentCountdownDeciSecond = countdownSecond * 10
+                binding.countdownTextView.text = String.format("%02d", countdownSecond)
             }
             setNegativeButton("취소", null)
 
@@ -85,11 +185,7 @@ class MainActivity : AppCompatActivity() {
             setPositiveButton("네") { _, _ ->
                 stop()
             }
-
-            setNegativeButton("아니요") { _, _ ->
-
-            }
-
+            setNegativeButton("아니요", null)
         }.show()
 
     }
