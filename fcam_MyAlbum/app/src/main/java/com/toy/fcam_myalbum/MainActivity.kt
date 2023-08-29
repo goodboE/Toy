9c918558d09152e93609505243026c531041be3e
HyunJin Ko <iamkokoko123@gmail.com>
2023-08-30 00:34:35 +0900

    draw ui - (1)

--- fcam_MyAlbum/app/src/main/java/com/toy/fcam_myalbum/MainActivity.kt
+++ fcam_MyAlbum/app/src/main/java/com/toy/fcam_myalbum/MainActivity.kt
@@ -1,11 +1,126 @@
 package com.toy.fcam_myalbum
 
+
+import android.Manifest.permission.*
+import android.app.Instrumentation.ActivityResult
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.toy.fcam_myalbum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageAdapter: ImageAdapter
+    private val imageLoadLauncher = registerForActivityResult(
+        ActivityResultContracts.GetMultipleContents()
+    ) { uriList ->
+        updateImages(uriList)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            checkPermission()
+        }
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        imageAdapter = ImageAdapter(object : ImageAdapter.ItemClickListener {
+            override fun onLoadMoreClick() {
+                checkPermission()
+            }
+        })
+
+        binding.imageRecyclerView.apply {
+            adapter = imageAdapter
+            layoutManager = GridLayoutManager(context, 2)
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+            shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE) -> {
+                showPermissionInfoDialog()
+            }
+            else -> {
+                requestPermission()
+            }
+        }
+
+    }
+
+    private fun loadImage() {
+        imageLoadLauncher.launch("image/*")
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("외부 저장소 권한 읽기 필요")
+            setNegativeButton("cancel", null)
+            setPositiveButton("OK") { _, _ ->
+                requestPermission()
+            }
+        }.show()
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                READ_EXTERNAL_STORAGE
+            ),
+            REQUEST_READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private fun updateImages(uriList: List<Uri>) {
+        Log.i("UpdateImages", "$uriList")
+        val images = uriList.map { ImageItems.Image(it) }
+        val updatedImages = imageAdapter.currentList.toMutableList().apply {
+            addAll(images)
+        }
+
+        imageAdapter.submitList(updatedImages)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+
+                val resultCode = grantResults.firstOrNull() ?: PackageManager.PERMISSION_DENIED
+                if (resultCode == PackageManager.PERMISSION_GRANTED) {
+                    loadImage()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_READ_EXTERNAL_STORAGE = 100
     }
 }
