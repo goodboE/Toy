7d5d18ee5365efd875407d75b961bda96275a511
ko <iamkokoko123@gmail.com>
2024-01-04 21:46:05 +0900

    init: setup android client

--- phone-book/server/src/main/java/ko/phonebook/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package ko.phonebook.repository;
-
-import ko.phonebook.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
